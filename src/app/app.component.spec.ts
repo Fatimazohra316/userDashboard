d0c765a134a55164221df1459afbcfd14b3cd156
Fatimazohra316 <fatimazohrai8is.com>
2023-01-24 15:53:36 +0500

    firstcommit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'adminScreen'`, () => {
+  it(`should have as title 'babysweet'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('adminScreen');
+    expect(app.title).toEqual('babysweet');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('adminScreen app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('babysweet app is running!');
   });
 });
